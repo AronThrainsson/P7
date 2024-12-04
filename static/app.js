55eeab077ad8db6e6d76fb51ecc67749db2c598a
AronThrainsson <siat@sundhedsdata.dk>
2024-12-04 15:18:47 +0100

    convert scanned text to lowercase

--- static/app.js
+++ static/app.js
@@ -1,11 +1,11 @@
 // Populate the search box with scanned text when on the home page
 document.addEventListener('DOMContentLoaded', () => {
-    const scannedText = localStorage.getItem('scannedText');
-    if (scannedText) {
-      const searchBox = document.getElementById('keyword');
-      if (searchBox) {
-        searchBox.value = scannedText; // Set the scanned text in the search box
-        localStorage.removeItem('scannedText'); // Clear the stored text
-      }
+  const scannedText = localStorage.getItem('scannedText');
+  if (scannedText) {
+    const searchBox = document.getElementById('keyword');
+    if (searchBox) {
+      searchBox.value = scannedText.toLowerCase(); // Convert to lowercase and set the value
+      localStorage.removeItem('scannedText'); // Clear the stored text
     }
-  });
+  }
+});
